c9cd3ae80ac0617131e72deb8cfbdf8b06c35a86
amothm80 <amo.1980@gmail.com>
2024-10-28 21:45:01 +0300

    game plan

---
+++ source.js
@@ -0,0 +1,25 @@
+/*
+
+user function: 
+    name
+    score
+
+game function:
+    add users
+    initialize game board
+    operate game board
+    get winner
+
+game board function:
+    initialize board array
+    get users
+    gameloop:
+        whos turn is it
+        input in a location
+            collision check
+        win condition check
+    return winner
+    
+    
+
+*/

16cf913fcfd35c872bf73f8c8f1e1991eeb4422e
Ahmed O <2990482+amothm80@users.noreply.github.com>
2024-10-29 09:03:06 +0300

    player, board functionality

--- source.js
+++ source.js
@@ -23,3 +23,53 @@ game board function:
     
 
 */
+
+
+function createPlayer(name){
+    let score = 0;
+    const getName = () => name;
+    const getScore = () => score;
+    const increaseScore = () => ++score;
+    const resetScore = () => score = 0;
+    return {getName,getScore,increaseScore,resetScore}
+}
+
+// function gameBoard(player1,player2){
+ function gameBoard(){
+    let turn = 0;
+    let player = 'X';
+    let board = ['','','','','','','','',''];
+
+    const resetGame = () => {
+        turn = 0;
+        player = 'X';
+        board = ['','','','','','','','',''];
+    }
+
+    const increaseTurn = () => turn++;
+
+    const getTurn = () =>{
+        let currentPlayer;
+        if (turn % 2  == 0){
+            currentPlayer = 'X';
+        }
+        else{
+            currentPlayer = 'O';
+        }
+        return player;
+    }
+
+    const checkWin = () =>{
+        if (board[0] == board[1] == board[2]){}
+    }
+
+    const playTurn = (location) => {
+        if ( board[location] != '' ){
+            return "Location is taken."
+        }else{
+            board[location] = player;
+        }
+        increaseTurn();
+        return board;
+    }
+
